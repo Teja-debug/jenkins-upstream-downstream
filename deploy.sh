32dc31dd536804f2c1827d636ca3031f309d9664
Teja-debug <tejadunaka@gmail.com>
2026-09-16 21:07:50 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "======================================"
+echo "       FINAL DEPLOYMENT STARTED"
+echo "======================================"
+
+echo "Deploying application..."
+
+sleep 2
+
+echo "Application deployment completed."
+
+echo "======================================"
+echo "       DEPLOYMENT SUCCESS"
+echo "======================================"
